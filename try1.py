6aacb88835af02360e485e8070857b68ab3c9800
Anuja Golechha <Anuja@Anujas-Mac.local>
2018-09-21 22:54:52 -0500

    updated try1.py

--- try1.py
+++ try1.py
@@ -2,5 +2,4 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import monotonically_increasing_id
 import sys
 
-spark = SparkSession.builder.appName("Try1").config("spark.some.config.option", "some-value").getOrCreate()
-
+spark = SparkSession.builder.appName("Try1").getOrCreate()
