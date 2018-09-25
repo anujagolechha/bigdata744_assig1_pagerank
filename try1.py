14ab6e33f0e5705fcdbb9f3c6eb62e0c786cf63d
Anuja Golechha <Anuja@Anujas-Mac.local>
2018-09-25 12:07:34 -0500

    persist linkinfo

--- try1.py
+++ try1.py
@@ -19,8 +19,8 @@ def calc(page2list, rank):
     n = len(page2list)
     for page2 in page2list:
         yield (page2, rank / n)
-        
-linkinfo = data.map(lambda x : extractIDs(x)).filter(lambda x : getValidIDs(x)).groupByKey()#.cache()
+
+linkinfo = data.map(lambda x : extractIDs(x)).filter(lambda x : getValidIDs(x)).groupByKey().cache()
 ranks = linkinfo.map(lambda url_neighbors : (url_neighbors[0], 1.0))
 
 n_iterations = 10
@@ -28,6 +28,5 @@ for _ in range(n_iterations):
     contributions = linkinfo.join(ranks).flatMap(
           lambda page1_page2list_rank1 : calc(page1_page2list_rank1[1][0], page1_page2list_rank1[1][1]))
     ranks = contributions.reduceByKey(add).mapValues(lambda contrib : 0.15 + (0.85 * contrib))
-    
+
 ranks.take(10)
-        
